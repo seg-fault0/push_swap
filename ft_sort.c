6bb3ae7ba4d3dc90b8a28ecfac24db44cfcb69dc
Walid Imam <wimam@e1r13p3.1337.ma>
2025-01-02 13:55:15 +0100

    make the code alitle more readable

--- ft_sort.c
+++ ft_sort.c
@@ -1,32 +1,41 @@
 #include "push_swap.h"
 
+void	ft_push_chunk(t_ps *arr, int limit, int average)
+{
+	int	i;
+
+	i = 0;
+	while (i < arr->size_a)
+	{
+		if(arr->arr_a[0] <= limit)
+		{
+			pb(arr);
+			if(arr->arr_b[0] <= average)
+				rb(arr);
+		}
+		else
+		{
+			ra(arr);
+			i++;
+		}
+	}
+}
+
 void	ft_push_to_b(t_ps *arr)
 {
 	int limit;
-	int i;
+	int	dif;
 	int average;
 
 	limit = ft_get_limit(arr, 20);
-	average = (limit - arr->arr_a[ft_get_min_index(arr->arr_a, arr->size_a)]) / 2;
+	dif = limit - arr->arr_a[ft_get_min_index(arr->arr_a, arr->size_a)];
+	average = limit - (dif / 2);
 	while(arr->size_a > 0)
 	{
-		i = 0;
-		while (i < arr->size_a)
-		{
-			if(arr->arr_a[0] <= limit)
-			{
-				pb(arr);
-				if(arr->arr_b[0] <= limit - average)
-					rb(arr);
-			}
-			else
-			{
-				ra(arr);
-				i++;
-			}
-		}
+		ft_push_chunk(arr, limit, average);
 		limit = ft_get_limit(arr, 20);
-		average = (limit - arr->arr_a[ft_get_min_index(arr->arr_a, arr->size_a)]) / 2;
+		dif = limit - arr->arr_a[ft_get_min_index(arr->arr_a, arr->size_a)];
+		average = limit - (dif / 2);
 	}
 
 }
