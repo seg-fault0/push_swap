c1d4c1c09ab5eaedbec15e4de8b87b15616c6734
Walid Imam <wimam@e1r9p14.1337.ma>
2025-01-06 19:19:01 +0100

    new push_a algo

--- chunck_sort.c
+++ chunck_sort.c
@@ -6,7 +6,7 @@
 /*   By: wimam <walidimam69gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 17:09:51 by wimam             #+#    #+#             */
-/*   Updated: 2025/01/06 17:59:41 by wimam            ###   ########.fr       */
+/*   Updated: 2025/01/06 19:18:11 by wimam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,3 +80,48 @@ void	chuck_sort(t_ps *arr)
 	ft_push_to_b(arr);
 	ft_push_to_a(arr);
 }
+
+/*
+
+void	ft_push_to_a(t_ps *arr)
+{
+	int	maxi;
+	int	max_nbr;
+	int second_max;
+	int lock;
+
+	lock = 0;
+	while (arr->size_b > 0)
+	{
+		maxi = ft_get_max_index(arr->arr_b, arr->size_b);
+		max_nbr = arr->arr_b[maxi];
+		second_max = ft_get_limit(arr->arr_b, arr->size_b, arr->size_b - 1);
+		while (1 == 1)
+		{
+			if (arr->arr_b[0] == max_nbr)
+			{
+				pa(arr);
+				if (lock == 1)
+				{
+					rra(arr);
+					lock = 0;
+				}
+				break ;
+			}
+			else if (arr->arr_b[0] == second_max && lock == 0)
+			{
+				pa(arr);
+				ra(arr);
+				lock = 1;
+				break ;
+			}
+			if (maxi > arr->size_b / 2)
+				rrb(arr);
+			else
+				rb(arr);
+		}
+	}
+}
+
+
+*/
